f7030da4dde7c6caef3e8cc4b87c565d19f28fa8
Ahmed Raza <123raza83@gmail.com>
2026-03-27 06:19:25 +0500

    Use standard Meta pixel bootstrap

--- src/components/TrackingScripts.jsx
+++ src/components/TrackingScripts.jsx
@@ -12,14 +12,23 @@ export default function TrackingScripts({
     <>
       {facebookPixelId ? (
         <>
-          <Script
-            id="facebook-pixel-loader"
-            src="https://connect.facebook.net/en_US/fbevents.js"
-            strategy="afterInteractive"
-          />
           <Script id="facebook-pixel-init" strategy="afterInteractive">
-            {`window.fbq=window.fbq||function(){(window.fbq.q=window.fbq.q||[]).push(arguments)};window._fbq=window._fbq||window.fbq;window.fbq.push=window.fbq;window.fbq.loaded=true;window.fbq.version='2.0';window.fbq.queue=window.fbq.queue||[];window.fbq('init', '${facebookPixelId}');window.fbq('track', 'PageView');`}
+            {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,
+'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '${facebookPixelId}');
+fbq('track', 'PageView');`}
           </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              alt=""
+              src={`https://www.facebook.com/tr?id=${facebookPixelId}&ev=PageView&noscript=1`}
+            />
+          </noscript>
         </>
       ) : null}
 
